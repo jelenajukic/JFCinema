eb6b5016f0ffb807db439a3d8c7219d497a54180
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-04-26 17:40:49 +0200

    changes

--- routes/tickets.js
+++ routes/tickets.js
@@ -4,10 +4,10 @@ const Cinema = require('../models/cinema')
 const Screening = require('../models/screening');
 const nodemailer = require("nodemailer");
 
-router.get('/confirmation', (req, res, next) => {
-  console.log("open it")
-  res.render('tickets/confirmation')
- });
+// router.get('/confirmation', (req, res, next) => {
+//   console.log("open it")
+//   res.render('tickets/confirmation')
+//  });
 
 router.get('/:id', (req, res, next) => {
   Screening.findOne({
@@ -16,16 +16,16 @@ router.get('/:id', (req, res, next) => {
     .populate('movieID')
     .populate('cinemaID')
     .then(screening => {
-      var screeningRoom = screening.roomID
-      var x = screening.cinemaID.rooms.forEach(room => {
-        if (room._id == screeningRoom) {
-          console.log("ok")
-          return room.name
-        }
-      })
-      screening.roomName = x
-      console.log(x);
-      console.log(screening.roomName);
+      // var screeningRoom = screening.roomID
+      // var x = screening.cinemaID.rooms.forEach(room => {
+      //   if (room._id == screeningRoom) {
+      //     console.log("ok")
+      //     return room.name
+      //   }
+      // })
+      // screening.roomName = x
+      // console.log(x);
+      // console.log(screening.roomName);
       res.render('tickets/ticket-selection_sp', screening)
 
     })
@@ -40,16 +40,16 @@ router.get('/:id/data', (req, res, next) => {
     .populate('movieID')
     .populate('cinemaID')
     .then(screening => {
-      var screeningRoom = screening.roomID
-      var x = screening.cinemaID.rooms.forEach(room => {
-        if (room._id == screeningRoom) {
-          console.log("ok")
-          return room.name
-        }
-      })
-      screening.roomName = x
-      console.log(x);
-      console.log(screening.roomName);
+      // var screeningRoom = screening.roomID
+      // var x = screening.cinemaID.rooms.forEach(room => {
+      //   if (room._id == screeningRoom) {
+      //     console.log("ok")
+      //     return room.name
+      //   }
+      // })
+      // screening.roomName = x
+      // console.log(x);
+      // console.log(screening.roomName);
       res.send(screening)
 
     })
@@ -58,8 +58,8 @@ router.get('/:id/data', (req, res, next) => {
 
 
 router.post("/:id", (req, res, next) => {
-  console.log(req.params.id);
-  console.log(req.user)
+  // console.log(req.params.id);
+  // console.log(req.user)
   Screening.findOne({
       _id: req.params.id,
 
@@ -77,7 +77,7 @@ router.post("/:id", (req, res, next) => {
         'seatPlan.$.userID': req.user
       }
     }))
-    .then(result => console.log(result))
+    //.then(result => console.log(result))
     .catch(error => console.log(error))
 })
 
@@ -87,12 +87,13 @@ router.post("/:id", (req, res, next) => {
  * Send e-mail route
  */
 router.post('/:id/send-email', (req, res, next) => {
-let messageSeats ="";
-  for(var i=0; i<req.body.reservation.length; i++){
-    messageSeats+=`Seat:${req.body.reservation[i].seatNo} in Row: ${req.body.reservation[i].row} //`
+
+  let messageSeats = "";
+  for (var i = 0; i < req.body.reservation.length; i++) {
+    messageSeats += `Seat:${req.body.reservation[i].seatNo} in Row: ${req.body.reservation[i].row} //`
   }
 
-  
+
   let subject = `Reservation for ${req.user.username} in JFCinema - ${req.body.screening.movieID.title}`
 
   console.log(subject)
@@ -103,7 +104,7 @@ let messageSeats ="";
   ${messageSeats}`;
 
   console.log(message);
- let transporter = nodemailer.createTransport({
+  let transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: 'JFCinema2019',
@@ -111,21 +112,21 @@ let messageSeats ="";
     }
   });
   transporter.sendMail({
-    from: '"JF Cinema - movie reservation 👻" <JFCinema2019@gmail.com>',
-    to: req.user.email, 
-    subject: subject, 
-    text: message,
-    html: `<b>${message}</b>`
-  })
-  .then(info => { 
-    debugger
-    res.redirect('/tickets/confirmation')
-    //res.render('cinema/details')
-    console.log("Works!")
-    // console.log(info)
+      from: '"JF Cinema - movie reservation 👻" <JFCinema2019@gmail.com>',
+      to: req.user.email,
+      subject: subject,
+      text: message,
+      html: `<b>${message}</b>`
+    })
+    .then(info => {
+      debugger
+      res.redirect('/tickets/confirmation')
+      //res.render('cinema/details')
+      console.log("Works!")
+      // console.log(info)
 
-})
-  .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 });
 
 
